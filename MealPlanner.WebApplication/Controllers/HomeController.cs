fc26f6fa9742857c29fa653622b559bd16d37606
Stephen Chan <stephen.s.c.chan@gmail.com>
2024-08-24 21:15:34 +0930

    Add controller for baseURL to redirect to swagger UI endpoint

---
+++ MealPlanner.WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MealPlanner.WebApplication.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Redirect("./swagger");
+        }
+    }
+}
